b066327928b528d02a6f69cd237244590807f754
Michael Latiolais <mike@latiolais.net>
2018-03-13 23:32:25 -0700

    Fixes #2 Fixes #3 Cleaned up code, fixed some weird errors where a "0x" prefix didn't resolve properly in the firmware.

--- eeprom_programmer.py
+++ eeprom_programmer.py
@@ -30,6 +30,34 @@ def getopts(argv):
         argv = argv[1:]  # Reduce the argument list by copying it starting from index 1.
     return (operation,opts)
 
+def write_byte(address, byte_val):
+  serial_string = "W" + address + ":" + byte_val + "\n"
+  print "Attempting: ", serial_string
+  try:
+    ser.write(serial_string)
+    read_val = ser.readline()
+  except:
+    print "Error writing to EEPROM"
+    sys.exit()
+  return read_val
+
+def read_byte(address):
+  serial_string = "R" + address + "\n"
+  print "Attempting: ", serial_string
+  try:
+    ser.write(serial_string)
+    read_val = ser.readline()
+    while "OK" in read_val:
+      read_val = ser.readline()
+  except:
+    print "Error writing to EEPROM"
+    sys.exit()
+  # Split on colon
+  #print "Read_val: ", read_val
+  byte_list = re.split(r'[:,]', read_val)
+  return byte_list[1][:2]
+  
+
 (operation,myargs) = getopts(argv)
 print "Operation: ",operation
 
@@ -40,7 +68,7 @@ current_index = 0
 ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = myargs['-s']
-ser.timeout = 2
+ser.timeout = .01 
 try:
   ser.open()
 except:
@@ -65,17 +93,17 @@ if operation == "write":
   rom_length = len(hexlist)
   print rom_length
   for x in range(0,rom_length):
-    hex_addr = hex(x)
+    hex_addr = format(x, 'x')
     val = hexlist[x]
-    serial_string = "W" + hex_addr + ":" + val + "\n"
-    print serial_string
-    try:
-      ser.write(serial_string)
-      read_val = ser.readline()
-    except:
-      print "Error writing to EEPROM"
-      sys.exit()
+    read_val = write_byte(hex_addr, val)
     print "Got: ",read_val
+    print "Now attempting to read back the value"
+    new_val = read_byte(hex_addr)
+    print "New value: ", new_val
+    if val != new_val:
+      print "ERROR: Bytes don't match"
+      exit(1)
+    
 
 if operation == "read":
   try:
@@ -84,8 +112,8 @@ if operation == "read":
     print "Error opening output file"
     sys.exit()
 
-  for x in range(0,int(myargs['-z'])):
-    serial_string = "R" + hex(x) + "\n"
+  for x in range(0,int(myargs['-z']),16):
+    serial_string = "R" + format(x, 'x') + "\n"
     print serial_string
     try:
       ser.write(serial_string)
@@ -94,12 +122,33 @@ if operation == "read":
       print "Error reading from EEPROM"
       sys.exit()
     print "Got:" , read_val
+    # Get response
+    response = ser.readline()
     byte_list = re.split(r'[:,]', read_val)
     fh.write(binascii.unhexlify(byte_list[1]))
-    x += 16
   fh.close()
 
 if operation == "compare":
   print "Compare"
+  # Grab binary file data
+  # Assume that the file size and the image are the same 
+  try:
+    with open(myargs['-i'], 'rb') as f:
+        # Slurp the whole file and efficiently convert it to hex all at once
+        hexdata = binascii.hexlify(f.read())
+  except:
+    print "Error opening input file"
+    sys.exit()
+  hexlist = map(''.join, zip(hexdata[::2], hexdata[1::2]))
+  # Go through the ROM contents and compare each 16 bytes
+  for x in range(0,int(myargs['-z'])):
+    address = format(x, 'x')
+    read_val = read_byte(address)
+    print "Got: ", read_val
+    image_val = hexlist[x]
+    if image_val == read_val:
+      print "Correct"
+    else:
+      print "ERROR: ROM does not match at address: ", address
 
 ser.close() 
